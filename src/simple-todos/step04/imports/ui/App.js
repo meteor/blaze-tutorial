3f50518b68a57981a23f277b5f2833047c6f6750
denyhs <denilsonh2014@gmail.com>
2021-03-29 15:10:28 -0400

    #597 - Migrate Blaze Tutorial
    - init step 04

---
+++ src/simple-todos/step04/imports/ui/App.js
@@ -0,0 +1,29 @@
+import { Template } from 'meteor/templating';
+import { TasksCollection } from "../api/TasksCollection";
+import './App.html';
+
+Template.body.helpers({
+  tasks() {
+    return TasksCollection.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+Template.form.events({
+  "submit .task-form"(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert a task into the collection
+    TasksCollection.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.text.value = '';
+  }
+})
