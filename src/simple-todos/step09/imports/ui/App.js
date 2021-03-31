6e9dc1ba1d2472a9aa8db6f5614c7a7dcd5c1d1f
denyhs <denilsonh2014@gmail.com>
2021-03-31 14:10:39 -0400

    #597 - Migrate Blaze Tutorial
    - step 09 init

---
+++ src/simple-todos/step09/imports/ui/App.js
@@ -0,0 +1,90 @@
+import { Template } from 'meteor/templating';
+import { TasksCollection } from "../db/TasksCollection";
+import { ReactiveDict } from 'meteor/reactive-dict';
+import './App.html';
+import './Task.js';
+import './Login.js';
+
+const HIDE_COMPLETED_STRING = "hideCompleted";
+
+const getUser = () => Meteor.user();
+const isUserLogged = () => !!getUser();
+
+const getTasksFilter = () => {
+  const user = getUser();
+
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+
+  const userFilter = user ? { userId: user._id } : {};
+
+  const pendingOnlyFilter = { ...hideCompletedFilter, ...userFilter };
+
+  return { userFilter, pendingOnlyFilter };
+}
+
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+})
+
+Template.body.events({
+  "click #hide-completed-button"(event, instance) {
+    const currentHideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+    instance.state.set(HIDE_COMPLETED_STRING, !currentHideCompleted);
+  },
+  'click .user'() {
+    Meteor.logout();
+  }
+});
+
+Template.body.helpers({
+  tasks() {
+    const instance = Template.instance();
+    const hideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+
+    const { pendingOnlyFilter, userFilter } = getTasksFilter();
+
+    if (!isUserLogged()) {
+      return [];
+    }
+
+    return TasksCollection.find(hideCompleted ? pendingOnlyFilter : userFilter, {
+      sort: { createdAt: -1 },
+    }).fetch();
+  },
+  hideCompleted() {
+    return Template.instance().state.get(HIDE_COMPLETED_STRING);
+  },
+  incompleteCount() {
+    if (!isUserLogged()) {
+      return '';
+    }
+
+    const { pendingOnlyFilter } = getTasksFilter();
+
+    const incompleteTasksCount = TasksCollection.find(pendingOnlyFilter).count();
+    return incompleteTasksCount ? `(${incompleteTasksCount})` : '';
+  },
+  isUserLogged() {
+    return isUserLogged();
+  },
+  getUser() {
+    return getUser();
+  }
+});
+
+Template.form.events({
+  "submit .task-form"(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert a task into the collection
+    Meteor.call('tasks.insert', text);
+
+    // Clear form
+    target.text.value = '';
+  }
+})
---
+++ src/simple-todos/step09/imports/ui/Task.js
@@ -0,0 +1,14 @@
+import { Template } from 'meteor/templating';
+
+
+import './Task.html';
+
+Template.task.events({
+  'click .toggle-checked'() {
+    // Set the checked property to the opposite of its current value
+    Meteor.call('tasks.setIsChecked', this._id, !this.isChecked);
+  },
+  'click .delete'() {
+    Meteor.call('tasks.remove', this._id);
+  },
+});
