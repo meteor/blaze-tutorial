2058ceb2e7e321a9355e3b5f1a9e02e678d1ca80
denyhs <denilsonh2014@gmail.com>
2021-04-01 14:02:57 -0400

    #597 - Migrate Blaze Tutorial
    - step 10 init

---
+++ src/simple-todos/step10/imports/ui/Login.js
@@ -0,0 +1,17 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import './Login.html';
+
+Template.login.events({
+  'submit .login-form'(e) {
+    e.preventDefault();
+
+    const target = e.target;
+
+    const username = target.username.value;
+    const password = target.password.value;
+
+    Meteor.loginWithPassword(username, password);
+  }
+});
+
